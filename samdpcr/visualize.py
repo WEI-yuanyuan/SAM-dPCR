1ad149819896d435d94c4c90864ef5f76027e5d4
Benny-XG <144464603+Benny-XG@users.noreply.github.com>
2024-12-08 15:15:58 +0800

    Add files via upload

--- samdpcr/visualize.py
+++ samdpcr/visualize.py
@@ -4,17 +4,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 import cv2
 from skimage.filters import threshold_otsu
-import numpy as np
- 
+
 matplotlib.use('TkAgg')
 
-def has_multiple_components(mask): # Function to determine if a mask has more than one connected component
-    # Apply connected components analysis
+def has_multiple_components(mask):
     num_labels, _, _, _ = cv2.connectedComponentsWithStats(mask, connectivity=8)
-    # If there's more than one label (excluding the background), it has multiple components
-    return num_labels > 2  # background label is 0, one object would make this 2
+    return num_labels > 2
 
-def show_bbox(image, bbox, classes, thickness=2): # Function to draw bounding boxes
+def show_bbox(image, bbox, classes, thickness=1):
     if classes == 'Positive':
         color = (0, 0, 255)
     elif classes == 'Negative':
@@ -27,7 +24,7 @@ def show_bbox(image, bbox, classes, thickness=2): # Function to draw bounding bo
 
     return image
 
-def findFalsyAnnotationsIndex(sorted_anns, removeArrayList): # Function to find annotations with multiple components
+def findFalsyAnnotationsIndex(sorted_anns, removeArrayList):
     for index_to_remove, ann in enumerate(sorted_anns):
         uint8_mask = (ann['segmentation'] * 255).astype(np.uint8)
         if uint8_mask is not None and has_multiple_components(uint8_mask):
@@ -35,13 +32,13 @@ def findFalsyAnnotationsIndex(sorted_anns, removeArrayList): # Function to find 
     
     return removeArrayList
 
-def removeFalsyAnnotations(sorted_anns, removeArrayList): # Function to remove annotations with multiple components
+def removeFalsyAnnotations(sorted_anns, removeArrayList):
     for i in sorted(removeArrayList, reverse=True):
-        # print(f"Removing: {sorted_anns[i]} at index {i}")
         del sorted_anns[i]
+    
     return sorted_anns
 
-def show(inputDirectory, outputDirectory, threshold_factor, mask_generator): # Function to classify droplet as bright or dark and visualize the results   
+def show(inputDirectory, outputDirectory, threshold_factor, mask_generator, min_area_ratio=0.5, max_area_ratio=1.5):
     if not os.path.exists(outputDirectory):
         os.makedirs(outputDirectory)
 
@@ -52,7 +49,7 @@ def show(inputDirectory, outputDirectory, threshold_factor, mask_generator): # F
         classifications = []
         annotations_to_remove = set()
 
-        if filename.endswith((".png", ".jpg", "tif")):  # Check for PNG and JPG files (change extensions as needed)
+        if filename.endswith((".png", ".jpg", "tif")):
             input_file_path = os.path.join(inputDirectory, filename)
             file_base_name = os.path.splitext(filename)[0]
             output_file_path = os.path.join(outputDirectory, file_base_name + "_DL.png")
@@ -65,38 +62,36 @@ def show(inputDirectory, outputDirectory, threshold_factor, mask_generator): # F
             hsv_targetImage[:, :, 2] = clahe.apply(hsv_targetImage[:, :, 2])
             hsv_targetImage = cv2.merge((hsv_targetImage[:, :, 0], hsv_targetImage[:, :, 1], hsv_targetImage[:, :, 2]))
             img_compensated = cv2.cvtColor(hsv_targetImage, cv2.COLOR_HSV2BGR)
-            # cv2.imshow('targetImage', hsv_targetImage[:, :, 2])
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
-
-            # cv2.imshow('targetImage', hsv_targetImage[:, :, 1])
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
 
             # generate annotations by SAM
             annotations = mask_generator.generate(img_compensated)
             if len(annotations) == 0:
                 return False
             
-            imgSatu = hsv_targetImage[:, :, 2]
-            imgSatu_np = np.array(imgSatu)
-            # imgValue_np = cv2.equalizeHist(imgValue_np)
-            # cv2.imshow('imgValue', imgValue_np)
-            # cv2.waitKey(0)
-            # cv2.destroyAllWindows()
+            imgValue = hsv_targetImage[:, :, 2]
+            imgValue_np = np.array(imgValue)
             
-            threshold = threshold_otsu(imgSatu_np) * threshold_factor
-            print(f"Threshold: {threshold}")
+            threshold = threshold_otsu(imgValue_np) * threshold_factor
 
+            sorted_anns = sorted(annotations, key=lambda x: x['area'], reverse=True)
 
-            sorted_anns = sorted(annotations, key=(lambda x: x['area']), reverse=True)
-            annotations_to_remove = findFalsyAnnotationsIndex(sorted_anns=sorted_anns, removeArrayList=annotations_to_remove)
-            sorted_anns = removeFalsyAnnotations(removeArrayList=annotations_to_remove, sorted_anns=sorted_anns)
+            # Calculate average area
+            average_area = np.mean([ann['area'] for ann in sorted_anns])
+            
+            # Set area thresholds based on average_area
+            min_area_threshold = average_area * min_area_ratio
+            max_area_threshold = average_area * max_area_ratio
 
-            for ann in sorted_anns:
+            # Filter annotations based on area thresholds
+            filtered_anns = [ann for ann in sorted_anns if min_area_threshold <= ann['area'] <= max_area_threshold]
+
+            annotations_to_remove = findFalsyAnnotationsIndex(sorted_anns=filtered_anns, removeArrayList=annotations_to_remove)
+            filtered_anns = removeFalsyAnnotations(removeArrayList=annotations_to_remove, sorted_anns=filtered_anns)
+
+            for ann in filtered_anns:
                 uint8_mask = (ann['segmentation'] * 255).astype(np.uint8)
                 _, uint8_mask = cv2.threshold(uint8_mask, 0, 255, cv2.THRESH_BINARY)
-                mean_value = cv2.mean(imgSatu, mask=uint8_mask)[0]
+                mean_value = cv2.mean(imgValue, mask=uint8_mask)[0]
 
                 bbox = ann['bbox']
 
@@ -111,7 +106,7 @@ def show(inputDirectory, outputDirectory, threshold_factor, mask_generator): # F
             cv2.imwrite(output_file_path, targetImage)
             print(f"Saved: {output_file_path}")
 
-            for ann in sorted_anns:
+            for ann in filtered_anns:
                 areas.append(ann['area'])
                 ious.append(ann['predicted_iou'])
                 s_scores.append(ann['stability_score'])
@@ -126,36 +121,4 @@ def show(inputDirectory, outputDirectory, threshold_factor, mask_generator): # F
                 np.array(['classification'] + classifications)
             ))
             np.savetxt(output_txt_file_path, data, fmt='%s')
-            #add total number of droplets
-            diameters=[]
-            for i in range(len(areas)):
-                diameters.append(np.sqrt(areas[i]/np.pi)*2)
-            diametermean=np.mean(diameters)
-            diameterstd=np.std(diameters)
-            # save diameters to txt file
-            new_output_txt_file_path = os.path.join(inputDirectory, file_base_name + "_diameter.txt")
-            with open(new_output_txt_file_path, 'a') as f:
-                for item in diameters:
-                    f.write("%s\n" % item)
-                f.close()
-            #add mean and variance of area
-            areamean=np.mean(areas)
-            areavar=np.var(areas)
-            total = len(classifications)
-            pos = classifications.count('Positive')
-            neg = classifications.count('Negative')
-            areastd=np.std(areas)
-            #create new txt file to inputfile path
-            new_output_txt_file_path = os.path.join(inputDirectory, file_base_name + "_stat.txt")
-            with open(new_output_txt_file_path, 'a') as f:
-                f.write(f"total: {total}\n")
-                f.write(f"positive: {pos}\n")
-                f.write(f"negative: {neg}\n")
-                f.write(f"mean: {areamean}\n")
-                f.write(f"variance: {areavar}\n")
-                f.write(f"standard deviation: {areastd}\n")
-                f.write(f"diameter mean: {diametermean}\n")
-                f.write(f"diameter standard deviation: {diameterstd}\n")
-                f.close()
             print(f"Saved: {output_txt_file_path}")
-
